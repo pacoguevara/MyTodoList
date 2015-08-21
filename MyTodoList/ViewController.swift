5d5d825c129a5b0db2efa1d882d2bf2e62e39994
Paco Guevara <pacoguevaraa@gmail.com>
2015-08-21 18:53:39 -0500

    Initial Commit

---
+++ MyTodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by Paco Guevara on 21/08/15.
+//  Copyright (c) 2015 Paco Guevara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
